a99351c5e7c2d7a3bfdecdd270b4ad91e4da5d8d
dwolffram <25206417+dwolffram@users.noreply.github.com>
2021-06-08 11:20:56 +0200

    submission 2021-06-07

--- US_ensemble.R
+++ US_ensemble.R
@@ -5,7 +5,7 @@ source("https://raw.githubusercontent.com/dwolffram/covid19-ensembles/master/sco
 source("https://raw.githubusercontent.com/dwolffram/covid19-ensembles/master/ensemble_methods.R")
 source("https://raw.githubusercontent.com/dwolffram/covid19-ensembles/master/ensemble_functions.R")
 
-forecast_date <- '2021-05-31'
+forecast_date <- '2021-06-07'
 
 df_train <- read_csv(paste0("data/df_train_", forecast_date, ".csv"), col_types = cols(
               forecast_date = col_date(format = ""),
@@ -16,7 +16,7 @@ df_train <- read_csv(paste0("data/df_train_", forecast_date, ".csv"), col_types 
               quantile = col_double(),
               value = col_double()
               )) %>%
-  #filter(location != 'US') %>%
+  filter(location != 'US') %>%
   as.data.frame()
 
 
@@ -29,7 +29,7 @@ df_test <- read_csv(paste0("data/df_test_", forecast_date, ".csv"), col_types = 
               quantile = col_double(),
               value = col_double()
             )) %>%
-  filter(location == 'US') %>%
+  filter(location != 'US') %>%
   as.data.frame()
 
 df_ensemble <- data.frame()
@@ -62,6 +62,7 @@ for (t in unique(df_test$target)){
 df_ensemble <- df_ensemble %>%
   select(-truth)
 
+
 file_name <- paste0("data/df_pred_", forecast_date, ".csv")
 file_name <- paste0("data/df_pred_US_", forecast_date, ".csv")
 
@@ -93,6 +94,8 @@ df <- read_csv("data/ensemble_data/submissions/2021-05-10-KITmetricslab-select_e
 df <- read_csv("data/ensemble_data/submissions/2021-05-17-KITmetricslab-select_ensemble.csv")
 df <- read_csv("data/ensemble_data/submissions/2021-05-24-KITmetricslab-select_ensemble.csv")
 df <- read_csv("data/ensemble_data/submissions/2021-05-31-KITmetricslab-select_ensemble.csv")
+
+df <- read_csv(paste0("submissions/", forecast_date, "-KITmetricslab-select_ensemble.csv"))
 
 
 plot_submission <- function(df, target="inc death", start_date="2020-11-01"){
