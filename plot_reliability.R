a258f5818ccac3166896654ffa1c1f3ce2fbac33
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-01-20 16:36:26 +0100

    added inset boxplots

--- plot_reliability.R
+++ plot_reliability.R
@@ -1,12 +1,21 @@
+library(tidyverse)
+
+Sys.setlocale("LC_ALL", "C")
+
 reldiag = function(x, y, alpha = 0.5, n_resamples = 999, digits = 3, region_level = 0.9){
   
   require(isotone)
-  pava = function(x, y){
-    # In case of ties,  gpava assigns mean instead of quantile, e.g.,
+  pava = function(x,y){
+    # In case of ties, isotone::gpava uses the conditional mean instead of quantile, try e.g.,
     # gpava(c(-1,-1,-1),c(-1,0,0),solver = weighted.median,ties = "secondary")
-    # the following step replaces y values with the respective quantile in case of ties
-    y = unlist(lapply(split(y, x),function(y) rep(quantile(y, alpha, type = 1), length(y))), use.names = FALSE)
-    return(gpava(x, y, solver = weighted.fractile, p = alpha, ties = "secondary")$x)
+    
+    # Wrong fix: The following step replaces y values with the respective quantile in case of ties
+    # y = unlist(lapply(split(y,x),function(y) rep(quantile(y,alpha,type = 1),length(y))),use.names = FALSE)
+    
+    # New fix: Use ranking of predictor values and break ties by ordering the corresponding instances in order of decreasing observations
+    ranking = match(1:length(x),order(x,y,decreasing = c(FALSE,TRUE)))
+    
+    return(gpava(ranking,y,solver = weighted.fractile,p = alpha,ties = "secondary")$x)
   }
   score = function(x, y) mean(2*(as.numeric(x >= y) - alpha)*(x-y))
   marg = function(x) quantile(x, alpha, type = 1)
@@ -117,6 +126,8 @@ reliability_diagram <- function(df, model = "COVIDhub-baseline",
     theme_gray(base_size = 18)    
 }
 
+eval_date <- '2022-01-03'
+
 df <- read.csv(paste0("evaluation/", eval_date, "_df_processed.csv")) %>%
   filter(location == "US")
 
@@ -189,7 +200,7 @@ reliability_diagram(df, models = c("COVIDhub-baseline", "COVIDhub-ensemble",
                     target = "1 wk ahead inc death", 
                     quantile = 0.5, n_resamples = 99)  
 
-ggsave("figures/rel_diag_national.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+#ggsave("figures/rel_diag_national.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
 
 # State level
 
@@ -201,4 +212,159 @@ reliability_diagram(df, models = c("COVIDhub-baseline", "COVIDhub-ensemble",
                     target = "1 wk ahead inc death", 
                     quantile = 0.5, n_resamples = 99) 
 
-ggsave("figures/rel_diag_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+#ggsave("figures/rel_diag_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+
+
+#### INSET
+
+models = c("COVIDhub-baseline", "COVIDhub-ensemble", "KITmetricslab-select_ensemble")
+target = "1 wk ahead inc death"
+quantile = 0.5
+n_resamples = 99
+
+df <- df %>%
+  filter(model %in% models,
+         target == !!target,
+         quantile == !!quantile)
+
+# compute recalibration, consistency band and score decomposition
+results <- df %>%
+  group_by(model) %>%
+  do(reldiag(.$value, .$truth, alpha = quantile, n_resamples = n_resamples))
+
+# summarize scores and create labels
+scores <- results %>%
+  group_by(model) %>%
+  distinct(across(score:pval_ucond)) %>%
+  mutate(label = paste0(c("QS ", "uMCB ","cMCB ","DSC ","UNC "),
+                        round(c(score, umcb, cmcb, dsc, unc), digits = 3),
+                        c("", paste0(" [p = ", round(pval_ucond, digits = 3),"]"), "", "", ""),
+                        c("", "", paste0(" [p = ", round(pval_cond, digits = 3),"]"), "", ""),
+                        collapse = "\n"))
+
+# needed to ensure square facets with equal x and y limits
+facet_lims <- results %>%
+  group_by(model) %>%
+  summarize(mn = min(c_across(c(x, x_rc, lower, upper))), 
+            mx = max(c_across(c(x, x_rc, lower, upper))))
+
+
+p <- ggplot(results, aes(x, x_rc)) +
+  facet_wrap("model", ncol=3) +
+  #geom_point(aes(x, y), alpha=0.2) +
+  geom_abline(intercept = 0 , slope = 1, colour="grey70") +
+  geom_point(color = "red", size=0.5) +
+  # geom_step(color = "red", direction = "vh") +    
+  geom_line(color = "red") + 
+  geom_smooth(aes(ymin = lower, ymax = upper), linetype = 0, stat = "identity", fill = "skyblue3") +
+  geom_rug(sides = "b", alpha = 0.2, size = 0.25) +
+  geom_blank(data = facet_lims, aes(x = mx, y = mx)) +
+  geom_blank(data = facet_lims, aes(x = mn, y = mn)) +
+  xlab("Forecast value") +
+  ylab("Conditional quantile") +
+  #labs(title = paste0(model, ":\n", target))  +
+  geom_label(data = scores, mapping = aes(x = -Inf, y = Inf, label = label),
+             size = 6*0.36, hjust = 0, vjust = 1, label.size = NA, alpha=0, label.padding = unit(1, "lines")) +
+  scale_x_continuous(guide = guide_axis(check.overlap = TRUE)) +
+  theme_bw(base_size = 11) +
+  theme(panel.grid.major = element_line(size = 0.05), 
+        panel.grid.minor = element_line(size = 0.05)) +
+  coord_fixed()
+
+inset <- ggplot(results, aes(x)) +
+  facet_wrap("model", ncol=3) +
+  geom_histogram(fill=NA, col="black") +
+  theme_classic() +
+  theme(axis.line.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position="none",
+        panel.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        plot.background=element_blank())
+
+ggdraw(p) +
+  draw_plot(inset, .45, .45, .5, .5)
+
+
+p <- ggplot(results, aes(x, x_rc, group=model)) +
+  facet_wrap(~model, ncol=3) +
+  #geom_point(aes(x, y), alpha=0.2) +
+  geom_abline(intercept = 0 , slope = 1, colour="grey70") +
+  #geom_point(color = "red", size=0.5) +
+  # geom_step(color = "red", direction = "vh") +    
+  geom_line(color = "red") + 
+  geom_smooth(aes(ymin = lower, ymax = upper), linetype = 0, stat = "identity", fill = "skyblue3") +
+  # geom_rug(sides = "b", alpha = 0.2, size = 0.25) +
+  geom_blank(data = facet_lims, aes(x = mx, y = mx)) +
+  geom_blank(data = facet_lims, aes(x = mn, y = mn)) +
+  xlab("Forecast value") +
+  ylab("Conditional quantile") +
+  #labs(title = paste0(model, ":\n", target))  +
+  geom_label(data = scores, mapping = aes(x = -Inf, y = Inf, label = label),
+             size = 6*0.36, hjust = 0, vjust = 1, label.size = NA, alpha=0, label.padding = unit(1, "lines")) +
+  scale_x_continuous(guide = guide_axis(check.overlap = TRUE)) +
+  theme_bw(base_size = 11) +
+  theme(panel.grid.major = element_line(size = 0.05), 
+        panel.grid.minor = element_line(size = 0.05)) +
+  coord_fixed()
+
+get_inset <- function(df, ...){
+  ggplot(df, aes(x)) +
+    geom_histogram(fill="gray", col="black", size=0.2, bins = 8) +
+    theme_classic( base_size=5.5) +
+    theme(axis.line.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.position="none",
+          panel.background=element_blank(),
+          panel.border=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.background=element_blank()) +
+    scale_x_continuous(guide = guide_axis(check.overlap = TRUE))
+}
+
+# get_inset(subset(results, model == "COVIDhub-ensemble"))
+
+annotation_custom2 <- function (grob, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf, data) 
+{
+  layer(data = data, stat = StatIdentity, position = PositionIdentity, 
+        geom = GeomCustomAnn,
+        inherit.aes = TRUE, params = list(grob = grob, 
+                                          xmin = xmin, xmax = xmax, 
+                                          ymin = ymin, ymax = ymax))
+}
+
+inset_plot <- get_inset(subset(results, model == "COVIDhub-ensemble"))
+
+p + annotation_custom2(grob=ggplotGrob(inset_plot), 
+                     data = subset(results, model == "COVIDhub-ensemble"),
+                     ymin = -4000, ymax=2500, xmin=10000, xmax=15000)
+
+
+
+
+get_annotation <- function(df, model){
+  inset_plot <- get_inset(df)
+  annotation_custom2(grob=ggplotGrob(inset_plot), 
+                     data = subset(df, model == unique(df$model)),
+                     ymin = 0, ymax=750, xmin=1500, xmax=2750)
+                     #ymin = -3000, ymax=2500, xmin=10000, xmax=15000)
+}
+
+insets <- results %>%
+  group_by(model) %>%
+  group_map(get_annotation, .keep=TRUE)
+
+         
+p + insets    
+
+ggsave("figures/rel_diag_inset_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+
